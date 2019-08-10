437d8ae33731e1a727e8b3e75860b01255a0be32
Jake <panamajake86@gmail.com>
2019-08-09 19:37:37 -0600

    First commit to start off the new branch.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,6 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+Jake
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
