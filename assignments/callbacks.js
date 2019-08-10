4b6f698b09d3ccd61f3900f0c68302d1dac2c8f5
Jake <panamajake86@gmail.com>
2019-08-09 21:23:21 -0600

    I finished the callbacks.js part of the project.  Learned some cool stuff from TL about another array method.  Loving this stuff.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,17 +2,17 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+ 
 
   //Given this problem: 
   
-  function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
-  }
+  // function firstItem(arr, cb) {
+  //   // firstItem passes the first item of the given array to the callback function.
+  // }
 
   // Potential Solution:
 
-  // Higher order function using "cb" as the call back
+ // Higher order function using "cb" as the call back
   function firstItem(arr, cb) {
     return cb(arr[0]);
   }
@@ -22,29 +22,70 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
     console.log(first)
   });
 
-*/
+
 
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length)
 }
+
+getLength(items, function(long){
+  console.log(long)
+}
+);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1])
 }
+
+last(items, function(final){
+  console.log(final)
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y)
 }
+
+sumNums(5, 2, function(seven){
+  console.log(seven)
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+    return cb(x * y)
 }
 
-function contains(item, list, cb) {
+multiplyNums(8, 4, function(thirtyTwo){
+  console.log(thirtyTwo)
+});
+
+/*function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+  // Pass true to the callback if it is, otherwise pass false.ff
+  return cb(list.includes(item));
+  console.log(list.includes(item))
+  if(list.includes(item)){
+    console.log("Yes")
+    cb("It is there!");
+  }else{
+    console.log("No")
+  }
+}*/
+function contains(item, list, cb){
+  return cb(list.includes(item));
 }
+
+contains("yo-yo", items, function (flag){
+  console.log(flag)
+  if(flag){
+    console.log("It's there!")
+  }else{
+    console.log("It's not there!")
+  }
+})
 
 /* STRETCH PROBLEM */
 
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,8 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-Jake
+const myOwnCreation = function(){
+  
+}
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
